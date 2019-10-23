435ad438bbfc37d5e663f250aab02b78e01b68b1
Peter Williams <pedroeldiablo@yahoo.com>
2019-10-23 22:37:44 +0100

    returning selected data from csv and assigning types (as all csv read as strings)

--- app.js
+++ app.js
@@ -8,3 +8,17 @@ d3.json('./countries.json', function(error, data) {
     .append('h3')
     .text(d => d.countryName);
 });
+
+d3.csv('./simplemaps-worldcities-basic.csv',function(row) {
+  if(+row.pop < 1000000) return;
+  return {
+    cityName: row.city,
+    countryCode: row.iso2,
+    population: +row.pop
+  };
+}, function(error, data){
+  if(error) throw error;
+
+  console.log(data);
+    
+});
